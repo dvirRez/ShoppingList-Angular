68df76cf18094f1369e5be7e56a4f5eb5e1b8c34
Dvir Rezenman <dvir.rez@gmail.com>
2018-03-25 15:47:45 +0300

    App component

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-home>
+    </app-home>
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
