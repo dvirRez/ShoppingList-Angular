17fb749ad6d6f3e5eb968198a56a65fc2325faa0
Dvir Rezenman <dvir.rez@gmail.com>
2018-03-25 15:49:04 +0300

    shopping-list component

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ListItemComponent } from '../list-item/list-item.component';
+
+@Component({
+  selector: 'app-shopping-list',
+  template: `
+    <div class="list_container">
+      <div class="sub_header">
+        <span>ADD YOUR ITEMS HERE</span>
+      </div>
+      <app-list-item
+         *ngFor="let listItem of listItems"
+         [item]="listItem"
+         (onChange)="onComplete.emit($event)"
+         (onRemove)="onDelete.emit($event)"
+         (onEditClick)="OnOpenDetailsPanel.emit($event)"
+      >
+      </app-list-item>
+      <div class="add_item_div">
+        <input class="add-item-input" name="label" (keyup.enter)="onAddedInputEnterKey.emit($event)">
+      </div>
+    </div>    
+  `,
+  styleUrls: ['./shopping-list.component.scss']
+})
+export class ShoppingListComponent implements OnInit {
+  @Input() listItems;
+  @Output() onComplete = new EventEmitter();
+  @Output() onDelete = new EventEmitter();
+  @Output() OnOpenDetailsPanel = new EventEmitter();
+  @Output() onAddedInputEnterKey = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
